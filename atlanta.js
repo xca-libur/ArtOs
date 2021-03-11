e66b8dc1eeffb44311ecb79172e45dcd384c80d1
xca-libur <79561225+xca-libur@users.noreply.github.com>
2021-03-11 21:18:56 +0600

    Update atlanta.js

--- atlanta.js
+++ atlanta.js
@@ -47,7 +47,7 @@ const init = async () => {
 		delete require.cache[require.resolve(`./events/${file}`)];
 	});
     
-	client.login(client.config.token); // Log in to the discord api
+	client.login(process.env.TOKEN); // Log in to the discord api
 
 	// connect to mongoose database
 	mongoose.connect(client.config.mongoDB, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
