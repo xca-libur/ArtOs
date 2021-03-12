f421687468b7e67aa5c4d1d08f2bcabf4d1fab0b
xca-libur <79561225+xca-libur@users.noreply.github.com>
2021-03-12 15:15:54 +0600

    Update help.js

--- commands/General/help.js
+++ commands/General/help.js
@@ -120,15 +120,7 @@ class Help extends Command {
 			}
 		}
         
-		embed.addField("\u200B", message.translate("misc:STATS_FOOTER", {
-			donateLink: "https://patreon.com/Androz2091",
-			dashboardLink: "https://dashboard.atlanta-bot.fr",
-			inviteLink: await this.client.generateInvite({
-				permissions: ["ADMINISTRATOR"]
-			}),
-			githubLink: "https://github.com/Androz2091",
-			supportLink: "https://discord.atlanta-bot.fr"
-		}));
+
 		embed.setAuthor(message.translate("general/help:TITLE", {
 			name: this.client.user.username
 		}), this.client.user.displayAvatarURL());
